172effde9cdc804fedf67c216766c0818940756d
luis remolina <74424028+luisremolina@users.noreply.github.com>
2022-05-02 15:03:53 -0500

    new database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA1wNakCfD8SIQE3shXONcS8DzivXQeDAU",
-    authDomain: "fir-crud-angular-20ab4.firebaseapp.com",
-    projectId: "fir-crud-angular-20ab4",
-    storageBucket: "fir-crud-angular-20ab4.appspot.com",
-    messagingSenderId: "407120924946",
-    appId: "1:407120924946:web:78b25694d199d23f0aa3cb",
-    measurementId: "G-FEM7RE50J9"
+    apiKey: "AIzaSyAv6rb8ewoxARRdOI9DVR_kwUTBCTwX8cA",
+    authDomain: "crud-basic-6c0d9.firebaseapp.com",
+    projectId: "crud-basic-6c0d9",
+    storageBucket: "crud-basic-6c0d9.appspot.com",
+    messagingSenderId: "847336295459",
+    appId: "1:847336295459:web:5f9af4117a2ec63b36b596",
+    measurementId: "G-19KH6D3XZ3"
   }
 };
